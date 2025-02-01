de44f0834008dc2ccd0c102d39c2bca40c1fd833
ggssDEVESH <deveshgokavarapu8@gmail.com>
2025-02-01 10:17:51 +0530

    git push "dev"

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send("Hello God!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello God!");
+});
 
-app.get('/twitter',(req,res) => {
-    res.send("this is my twitter")
-})
+app.get("/twitter", (req, res) => {
+  res.send("this is my twitter");
+});
 
-app.get('/login', (req,res) => {
-    res.send("<h1>veera venkata naga surya chandhra gandhipathi sri sai devesh gokavarapu </h1>")
-})
+app.get("/login", (req, res) => {
+  res.send(
+    "<h1>veera venkata naga surya chandhra gandhipathi sri sai devesh gokavarapu </h1>"
+  );
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`shivaya govindya ${process.env.PORT}`)
-})
+  console.log(`shivakesava ${process.env.PORT}`);
+});
